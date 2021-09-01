cfabe4e3d49e45d8297071a550b9b32711ff2cb7
yesseruser <yesseruseryt@gmail.com>
2021-09-01 13:42:53 +0200

    Přidejte soubory projektu.

---
+++ Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UsefulTools;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] testStuff = { 1, 2, 4, -50, -2343214, Convert.ToDecimal(111.4343), 2509 };
+            decimal max = Tools.Aritmetic.Max(testStuff);
+            decimal min = Tools.Aritmetic.Min(testStuff);
+            decimal sum = Tools.Aritmetic.Sum(testStuff);
+            decimal diameter = Tools.Aritmetic.Diameter(testStuff);
+            Console.WriteLine(max + " " + min + " " + sum + " " + diameter);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UsefulTools/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace UsefulTools
+{
+    public static class Tools
+    {
+        public static class Aritmetic
+        {
+            public static decimal Add(decimal num1, decimal num2) => num1 + num2;
+            public static decimal Substract(decimal num1, decimal num2) => num1 - num2;
+            public static decimal Multiply(decimal num1, decimal num2) => num1 * num2;
+            public static decimal Divide(decimal num1, decimal num2) => num1 / num2;
+            public static decimal Modulo(decimal num1, decimal num2) => num1 % num2;
+            public static decimal Diameter(decimal[] nums)
+            {
+                decimal sum = Sum(nums);
+                int count = Count(nums);
+                decimal result = sum / count;
+                return result;
+            }
+            public static decimal Sum(decimal[] nums)
+            {
+                decimal sum = 0;
+                foreach (decimal item in nums)
+                {
+                    sum += item;
+                }
+                return sum;
+            }
+            public static int Count<T>(T[] nums) => nums.Length;
+            public static decimal Max(decimal[] nums)
+            {
+                decimal max = nums[0];
+                for(int i = 0; i < Count(nums); i++)
+                {
+                    foreach (int item in nums)
+                    {
+                        if(max < item)
+                        {
+                            max = item;
+                        }
+                    }
+                }
+                return max;
+            }
+            public static decimal Min(decimal[] nums)
+            {
+                decimal min = nums[0];
+                for (int i = 0; i < Count(nums); i++)
+                {
+                    foreach (int item in nums)
+                    {
+                        if (min > item)
+                        {
+                            min = item;
+                        }
+                    }
+                }
+                return min;
+            }
+        }
+    }
+}
