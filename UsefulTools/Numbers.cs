9862d2f850fafd8c3c1bae4f896c5e220a786430
yesseruser <yesseruseryt@gmail.com>
2021-09-03 23:47:15 +0200

    Added some structs

--- UsefulTools/Numbers.cs
+++ UsefulTools/Numbers.cs
@@ -4,6 +4,50 @@
     {
         public static class Numbers
         {
+            public struct Size2D
+            {
+                public double width;
+                public double heigth;
+                public Size2D(double width, double heigth)
+                {
+                    this.width = width;
+                    this.heigth = heigth;
+                }
+            }
+            public struct Size3D
+            {
+                public double width;
+                public double heigth;
+                public double length;
+                public Size3D(double width, double heigth, double length)
+                {
+                    this.width = width;
+                    this.heigth = heigth;
+                    this.length = length;
+                }
+            }
+            public struct Coordinates2D
+            {
+                public double x;
+                public double y;
+                public Coordinates2D(double x, double y)
+                {
+                    this.x = x;
+                    this.y = y;
+                }
+            }
+            public struct Coordinates3D
+            {
+                public double x;
+                public double y;
+                public double z;
+                public Coordinates3D(double x, double y, double z)
+                {
+                    this.x = x;
+                    this.y = y;
+                    this.z = z;
+                }
+            }
             public static readonly double pi = 3.14159265359;
             public static decimal Add(decimal num1, decimal num2) => num1 + num2;
             public static decimal Substract(decimal num1, decimal num2) => num1 - num2;
