1850c872b1e51a36042f88d7834628a517da85b5
yesseruser <yesseruseryt@gmail.com>
2021-09-03 18:41:14 +0200

    Made a file for every sub-class

---
+++ UsefulTools/Texts.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace UsefulTools
+{
+    public partial class Tools
+    {
+        public static class Texts
+        {
+            public static void PrintSomething(string text)
+            {
+                Console.WriteLine(text);
+            }
+            public static void PrintSomethingWithoutEndLine(string text)
+            {
+                Console.WriteLine(text);
+            }
+            public static string JoinStrings(string[] strings)
+            {
+                string output = "";
+                foreach (string item in strings)
+                {
+                    output += item;
+                }
+                return output;
+            }
+            public static string JoinChars(char[] chars)
+            {
+                string output = "";
+                foreach (char item in chars)
+                {
+                    output += item;
+                }
+                return output;
+            }
+            public static string Lower(string text) => text.ToLower();
+            public static string Upper(string text) => text.ToUpper();
+            public static bool IsNullOrWhitespace(string text) => IsNullOrWhitespace(text);
+        }
+    }
+}
