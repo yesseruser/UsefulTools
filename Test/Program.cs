5a3e7f0cfbd58e848a368edfae2ec2e55321d433
yesseruser <yesseruseryt@gmail.com>
2021-09-04 10:25:40 +0200

    Updated the test

--- Test/Program.cs
+++ Test/Program.cs
@@ -7,18 +7,21 @@ namespace Test
     {
         private static void Main(string[] args)
         {
+            const double pi = Tools.Numbers.pi;
             double[] testStuff = { 1, 2, 4, -50, -2343214, Convert.ToDouble(111.4343), 2509 };
             double max = Tools.Numbers.Max(testStuff);
             double min = Tools.Numbers.Min(testStuff);
             double sum = Tools.Numbers.Sum(testStuff);
             double diameter = Tools.Numbers.Diameter(testStuff);
             Console.WriteLine(max + " " + min + " " + sum + " " + diameter);
-            Console.ReadKey();
             char[] chars = { 'A', 'b', 'c', 'd', 'e', 'f', 'g' };
             string idk = Tools.Texts.JoinChars(chars);
             Console.WriteLine(idk);
             int why = 2;
             Console.WriteLine(Tools.Numbers.ToDecimal(why));
+            Console.WriteLine(pi);
+
+            Console.ReadKey();
         }
     }
 }
