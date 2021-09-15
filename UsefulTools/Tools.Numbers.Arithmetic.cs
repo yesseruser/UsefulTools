ec5c44ed431f838a6760bf45ab858b58f7d55054
yesseruser <yesseruseryt@gmail.com>
2021-09-15 09:19:01 +0200

    Made a new class file: Tools.Numbers.Arithmetic.cs and moved arithmetic functions there

---
+++ UsefulTools/Tools.Numbers.Arithmetic.cs
@@ -0,0 +1,47 @@
+﻿namespace UsefulTools
+{
+    public partial class Tools
+    {
+        public static partial class Numbers
+        {
+            public static class Arithmetic
+            {
+                /// <summary>
+                /// Adds two nums together.
+                /// </summary>
+                /// <param name="num1">number 1</param>
+                /// <param name="num2">number 2</param>
+                /// <returns>The added numbers</returns>
+                public static double Add(double num1, double num2) => num1 + num2;
+                /// <summary>
+                /// Substracts two nums.
+                /// </summary>
+                /// <param name="num1">number 1</param>
+                /// <param name="num2">number 2</param>
+                /// <returns>The substracted numbers</returns>
+                public static double Substract(double num1, double num2) => num1 - num2;
+                /// <summary>
+                /// Multiplies two nums.
+                /// </summary>
+                /// <param name="num1">number 1</param>
+                /// <param name="num2">number 2</param>
+                /// <returns>The multiplied numbers</returns>
+                public static double Multiply(double num1, double num2) => num1 * num2;
+                /// <summary>
+                /// Divides two numbers.
+                /// </summary>
+                /// <param name="num1">number 1</param>
+                /// <param name="num2">number 2</param>
+                /// <returns>The divided numbers</returns>
+                public static double Divide(double num1, double num2) => num1 / num2;
+                /// <summary>
+                /// Returns the remainder of dividing two numbers.
+                /// </summary>
+                /// <param name="num1">number 1</param>
+                /// <param name="num2">number 2</param>
+                /// <returns>The remainder</returns>
+                public static double Modulo(double num1, double num2) => num1 % num2;
+            }
+        }
+    }
+}
--- UsefulTools/Tools.Numbers.cs
+++ UsefulTools/Tools.Numbers.cs
@@ -4,7 +4,7 @@ namespace UsefulTools
 {
     public partial class Tools
     {
-        public static class Numbers
+        public static partial class Numbers
         {
             /// <summary>
             /// A 2D size struct.
@@ -70,41 +70,8 @@ namespace UsefulTools
             /// The pi constant.
             /// </summary>
             public const double pi = 3.14159265359;
-            /// <summary>
-            /// Adds two nums together.
-            /// </summary>
-            /// <param name="num1">number 1</param>
-            /// <param name="num2">number 2</param>
-            /// <returns>The added numbers</returns>
-            public static double Add(double num1, double num2) => num1 + num2;
-            /// <summary>
-            /// Substracts two nums.
-            /// </summary>
-            /// <param name="num1">number 1</param>
-            /// <param name="num2">number 2</param>
-            /// <returns>The substracted numbers</returns>
-            public static double Substract(double num1, double num2) => num1 - num2;
-            /// <summary>
-            /// Multiplies two nums.
-            /// </summary>
-            /// <param name="num1">number 1</param>
-            /// <param name="num2">number 2</param>
-            /// <returns>The multiplied numbers</returns>
-            public static double Multiply(double num1, double num2) => num1 * num2;
-            /// <summary>
-            /// Divides two numbers.
-            /// </summary>
-            /// <param name="num1">number 1</param>
-            /// <param name="num2">number 2</param>
-            /// <returns>The divided numbers</returns>
-            public static double Divide(double num1, double num2) => num1 / num2;
-            /// <summary>
-            /// Returns the remainder of dividing two numbers.
-            /// </summary>
-            /// <param name="num1">number 1</param>
-            /// <param name="num2">number 2</param>
-            /// <returns>The remainder</returns>
-            public static double Modulo(double num1, double num2) => num1 % num2;
+            
+            
             /// <summary>
             /// Returns the diameter of given numbers.
             /// </summary>
