b45ade271e2f492fa6b9c9a4ded84ea9bac9d580
yesseruser <yesseruseryt@gmail.com>
2021-11-03 15:07:13 +0100

    Added True and false properties

--- UsefulTools/Tools.Boolean.cs
+++ UsefulTools/Tools.Boolean.cs
@@ -4,6 +4,8 @@
     {
         public static class Boolean
         {
+            public static bool True { get => true; }
+            public static bool False { get => false; }
             public static bool Invert(bool input) => !input;
         }
     }
