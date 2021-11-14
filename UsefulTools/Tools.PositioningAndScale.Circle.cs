3bc0b27db4fed96ae0b07a6c7aa795527d4beca8
yesseruser <yesseruseryt@gmail.com>
2021-11-14 21:14:43 +0100

    Moved Circle to a new class

---
+++ UsefulTools/Tools.PositioningAndScale.Circle.cs
@@ -0,0 +1,33 @@
+﻿namespace UsefulTools
+{
+    public partial class Tools
+    {
+        public static partial class PositioningAndScale
+        {
+            public class Circle : Rectangle
+            {
+                /// <summary>
+                /// The radius of the circle.
+                /// </summary>
+                public double Radius
+                {
+                    get
+                    {
+                        return size.width / 2;
+                    }
+                }
+
+                /// <summary>
+                /// The diameter of the circle.
+                /// </summary>
+                public double Diameter
+                {
+                    get
+                    {
+                        return Radius * 2;
+                    }
+                }
+            }
+        }
+    }
+}
--- UsefulTools/Tools.PositioningAndScale.Rectangle.cs
+++ UsefulTools/Tools.PositioningAndScale.Rectangle.cs
@@ -45,30 +45,6 @@
 
                 }
             }
-            public class Circle : Rectangle
-            {
-                /// <summary>
-                /// The radius of the circle.
-                /// </summary>
-                public double Radius
-                {
-                    get
-                    {
-                        return size.width / 2;
-                    }
-                }
-
-                /// <summary>
-                /// The diameter of the circle.
-                /// </summary>
-                public double Diameter
-                {
-                    get
-                    {
-                        return Radius * 2;
-                    }
-                }
-            }
         }
     }
 }
