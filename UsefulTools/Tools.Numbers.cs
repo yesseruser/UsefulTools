9724414397180d25c234559922fa48b6a6c5a6e1
yesseruser <yesseruseryt@gmail.com>
2022-03-26 22:54:33 +0100

    Added a method to check if an intenger is prime.

--- UsefulTools/Tools.Numbers.cs
+++ UsefulTools/Tools.Numbers.cs
@@ -164,6 +164,25 @@
             /// <param name="number"></param>
             /// <returns></returns>
             public static double Cbrt(double number) => Math.Cbrt(number);
+
+            public static bool IsPrime(int number)
+            {
+                if (number < 2) return false;
+                if (number == 2) return true;
+                if (number % 2 == 0) return false;
+
+                int max = (int)Math.Sqrt(number);
+
+                for (int i = 3; i <= max; i += 2)
+                {
+                    if (number % i == 0)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
         }
     }
 }
